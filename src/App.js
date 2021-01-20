908e7ba2f6bb9af4f65344684ee63902c891a6e6
ahmedseragcodes <ahmedserag1@gmail.com>
2021-01-20 17:15:47 -0500

    created file structure along with axios request returning photo of the day and accompanying information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { API_KEY } from "./constants/index.js";
+import axios from "axios";
 
 function App() {
+
+  
+  const [date, setDate]=useState("");
+  
+  const [explanation, setExplanation]=useState("");
+
+  const [title, setTitle]=useState("");
+  
+  const [dailyPhoto, setDailyPhoto]=useState("");
+
+  useEffect(function(){
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+    .then(function(res){
+      console.log(res);
+      setDailyPhoto(res.data.url);
+      setDate(res.data.date);
+      setExplanation(res.data.explanation);
+      setTitle(res.data.title);
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+  },[])
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>NASA Astronomical Photo Of The Day</h1>
+      <h2>{title}</h2>
+      <img src={dailyPhoto} alt="NASA's astronomical photo of the day, typically displaying photos of stars, planets, moons, galaxies and so on" />
+      <p>{date}</p>
+      <p>{explanation}</p>
     </div>
   );
 }

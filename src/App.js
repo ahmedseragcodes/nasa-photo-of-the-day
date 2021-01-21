b47d6b10a6d020b2e98bb72a70b459f698fef082
ahmedseragcodes <ahmedserag1@gmail.com>
2021-01-21 17:49:59 -0500

    utilized styled components to change styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { API_KEY } from "./constants/index.js";
 import axios from "axios";
+import styled from "styled-components";
 
-function App() {
+
+
+
+function App(props) {
 
   
   const [date, setDate]=useState("");
@@ -30,14 +34,30 @@ function App() {
 
 
   return (
+    <NasaContainer >
     <div className="App">
       <h1>NASA Astronomical Photo Of The Day</h1>
       <h2>{title}</h2>
-      <img src={dailyPhoto} alt="NASA's astronomical photo of the day, typically displaying photos of stars, planets, moons, galaxies and so on" />
+      <NasaPhoto src={dailyPhoto} alt="NASA's astronomical picture of the day, typically displaying photos of stars, planets, moons, galaxies and so on" />
       <p>{date}</p>
       <p>{explanation}</p>
     </div>
+    </NasaContainer>
   );
 }
+
+const NasaContainer=styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+text-align: center;
+background-color: #0B3D91;
+color: #FC3D21;
+`
+
+const NasaPhoto=styled.img`
+height: 20rem;
+width: 20rem;
+`
 
 export default App;
